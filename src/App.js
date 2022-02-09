e71714289cc58e72ade0a31d9186cf506a4a196c
sangmin.lee <sangmin.lee@cenacle.com>
2022-02-09 17:48:24 +0900

    간단한 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useState, useEffect } from "react";
 
 function App() {
+  const results = ["바위", "가위", "보"];
+
+  const [result, setResult] = useState(results[0]);
+
+  window.onkeydown = (e) => {
+    const key = e.key;
+    if(key === 'q') {
+      alert("바위!");
+    } else if(key === 'w') {
+      alert("가위!");
+    } else if(key === 'e') {
+      alert("보!");
+    } else {
+      alert("잘못된 입력입니다...");
+    }
+  }
+
+  useEffect(() => {
+    setInterval(() => {
+      setResult(results[Math.floor(Math.random() * results.length)]);
+    }, 10);
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {result}
     </div>
   );
 }
