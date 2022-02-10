469af0ddf18e229664597398c8dee18c42a02210
sangmin.lee <sangmin.lee@cenacle.com>
2022-02-10 14:43:04 +0900

    alert 창으로 구현 된 부분 화면에 표시되도록 수정

--- src/App.js
+++ src/App.js
@@ -1,33 +1,68 @@
 import { useCallback, useState, useEffect } from "react";
 
 function App() {
-  const results = ["바위", "가위", "보"];
+  const choices = ["바위", "가위", "보"];
 
-  const [result, setResult] = useState(results[0]);
+  const [resultIdx, setResultIdx] = useState(choices[0]);
+  const [eventFunction, setEventFunction] = useState(null);
+  const [myChoiceIdx, setMyChoiceIdx] = useState(null);
+  const [result, setResult] = useState("");
 
   window.onkeydown = (e) => {
-    const key = e.key;
-    if(key === 'q') {
-      alert("바위!");
-    } else if(key === 'w') {
-      alert("가위!");
-    } else if(key === 'e') {
-      alert("보!");
-    } else {
-      alert("잘못된 입력입니다...");
-    }
+    clearInterval(eventFunction)
+
+    // const delayFunction = setInterval(() => {
+    //   setResultIdx(Math.floor(Math.random() * choices.length));
+    //   console.log(resultIdx);
+    // }, 100);
+    
+    setTimeout(() => {
+      // clearInterval(delayFunction)
+      let choiceIdx;
+
+      const key = e.key;
+      if(key === 'q') {
+        choiceIdx = 0;
+        setMyChoiceIdx(choiceIdx);
+      } else if(key === 'w') {
+        choiceIdx = 1;
+        setMyChoiceIdx(choiceIdx);
+      } else if(key === 'e') {
+        choiceIdx = 2;
+        setMyChoiceIdx(choiceIdx);
+      } else {
+        throw "잘못된 입력입니다...";
+      }
+
+      const comparison = ((choiceIdx - resultIdx) % 3 + 3) % 3;
+
+      if(comparison === 2) {
+        setResult("win");
+      } else if(comparison === 1) {
+        setResult("lose");
+      } else {
+        setResult("tie");
+      }
+    }, 1000);
+
   }
 
   useEffect(() => {
-    setInterval(() => {
-      setResult(results[Math.floor(Math.random() * results.length)]);
-    }, 10);
+    setEventFunction(setInterval(() => {
+      setResultIdx(Math.floor(Math.random() * choices.length));
+    }, 10));
 
   }, []);
 
   return (
     <div>
-      {result}
+      {choices[resultIdx]}
+      <div>
+        {myChoiceIdx !== null ? `당신의 선택은 : ${choices[myChoiceIdx]}` : null}
+      </div>
+      <div>
+        {result}  
+      </div>
     </div>
   );
 }
